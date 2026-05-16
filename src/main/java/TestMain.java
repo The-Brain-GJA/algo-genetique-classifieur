d93e305ac883b91619d2def875efed089f0ba938
Gaël Portable <gael.jaffre@yahoo.fr>
2026-05-16 08:40:22 +0200

    MAJ

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -1,6 +1,19 @@
+import outils.Timer;
 
 public class TestMain {
+	
+	/*
+	 * set datafile separator comma
+	 * plot 'file_15.csv' skip 1 using 1:2
+	 * 
+	 */
+	
 	public static void main(String[] args) {
+		
+		Timer timer = Timer.getTimer("Main");
+		
 		System.out.println("Test");
+		
+		timer.afficher();
 	}
 }
